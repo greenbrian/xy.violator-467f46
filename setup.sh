a16b01e7404b414904f36e7b121dd22044545677
greenbrian <greenbrian@gmail.com>
2016-10-08 11:29:38 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # debug
 set -xv
 
+# update brew
+brew upate
+brew upgrade
+
 # mac setup
 
 brew cask install atom
@@ -14,3 +18,16 @@ brew install python
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install vagrant-manager
+brew cask install iterm2
+brew install bash-completion
+
+cat << EOF > $HOME/.vimrc
+:set nocompatible
+syntax on
+EOF
+
+cat << EOF > $HOME/.bash_profile
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
+EOF
