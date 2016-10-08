a650a097b4cc0b054267f3365c628b538824c967
greenbrian <greenbrian@gmail.com>
2016-10-08 11:12:43 -0500

    setup.sh
    setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# debug
+set -xv
+
+# mac setup
+
+brew cask install atom
+brew cask install whatsapp
+brew install vim
+brew install git
+brew install curl
+brew install python
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install vagrant-manager
